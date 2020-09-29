501e030ac062b932967d5c55573e9d16c84c06af
StephenRaymond <55437484+StephenRaymond@users.noreply.github.com>
2020-09-29 16:38:09 +0530

    .

---
+++ Beginner/FirstFiveMultiples.py
@@ -0,0 +1,6 @@
+n=int(input())
+m=n
+i=1
+for x in range(n,n+5):
+    print(m*i)
+    i=i+1
---
+++ Beginner/MinToHr.py
@@ -0,0 +1,11 @@
+n=int(input())
+hr=0
+min=0
+if n<60:
+    print(hr,end=' ')
+    print(min+n)
+else:
+    a=n//60
+    print(a,end=' ')
+    b=n%60
+    print(b)
